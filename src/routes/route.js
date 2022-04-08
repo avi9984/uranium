ad3a80eb858bae85296baf5814dba8c2fec160ca
Avinash Kumar <avinashkumar151199@gmail.com>
2022-04-08 23:32:51 +0530

    avi1

--- src/routes/route.js
+++ src/routes/route.js
@@ -3,8 +3,61 @@ const express = require('express');
 const router = express.Router();
 
 router.get('/test-me', function (req, res) {
-    res.send('My first ever api!')
+
+    // let a={msg:"My first ever api!"}
+
+
+    res.send({msg:"My first ever api!"})
 });
+
+
+router.get('/test-me1', function (req, res) {
+
+    // let a={msg:"My first ever api!"}
+
+
+    res.send({msg:"My first ever API !"})
+});
+
+router.get('/test-me1', function (req, res) {
+
+    // let a={msg:"My first ever api!"}
+
+
+    res.send({msg:"My first ever API !"})
+});
+
+router.get('/test-me4', function (req, res) {
+
+    // let a={msg:"My first ever api!"}
+
+
+    res.send({msg:"My first ever API this is another !", name:"funtionup", age:40})
+});
+
+router.post('/test-post1', function (req, res) {
+
+    // let a={msg:"My first ever api!"}
+
+
+    res.send({msg:"hi"})
+});
+
+router.post('/test-post2', function (req, res) {
+    let data=req.body
+    console.log(data)
+
+    res.send({msg:"hi guy's"})
+});
+
+router.post('/test-post2', function (req, res) {
+    let data=req.body
+    console.log(data)
+
+    res.send({msg:"hi guy's"})
+});
+
+
 
 module.exports = router;
 // adding this comment for no reason
