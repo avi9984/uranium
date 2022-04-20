461ade7e4df4b7ee75b5d17e880187ce1a57af30
Avinash Kumar <avinashkumar151199@gmail.com>
2022-04-20 16:49:00 +0530

    middleware-1 Done

--- src/index.js
+++ src/index.js
@@ -8,20 +8,49 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
 
-mongoose.connect("mongodb+srv://functionup-cohort:G0Loxqc9wFEGyEeJ@cluster0.rzotr.mongodb.net/Pritesh8769811-DB?retryWrites=true&w=majority", {
+
+mongoose.connect("mongodb+srv://Avi9984:edmXH1jaB63lQyo8@cluster0.qfc4n.mongodb.net/Avi99", {
     useNewUrlParser: true
 })
 .then( () => console.log("MongoDb is connected"))
 .catch ( err => console.log(err) )
 
-app.use (
-    function (req, res, next) {
-        console.log ("inside GLOBAL MW");
-        next();
-  }
-  );
+// app.use (
+//     function (req, res, next) {
+//         console.log ("inside GLOBAL MW");
+//         next();
+//   }
+//   );
+
+// const moment = require('moment');
+// let time = moment();
+// app.use (function (req, res, next) {
+//     let url= req.Url
+//     let a= req.ip
+
+//     console.log(time, url , a);
+//     next()
+// })
+
+app.use(function(req, res, next){
+    let currentDate= new Date();
+    let deteTime= currentDate.getDate() + " "
+
+    +(currentDate.getMonth()+1) + " "
+    +currentDate.getFullYear() + " "
+    + currentDate.getHours() + " "
+    + currentDate.getMinutes()+ " "
+    + currentDate.getSeconds()+ " "
+
+    let ip=req.ip
+    let url= req.orignalUrl
+
+    console.log(`${deteTime} ${ip} ${url}`)
+})
 
 app.use('/', route);
+
+
 
 
 app.listen(process.env.PORT || 3000, function () {
--- src/routes/route.js
+++ src/routes/route.js
@@ -5,9 +5,22 @@ const UserController= require("../controllers/userController")
 const BookController= require("../controllers/bookController")
 const commonMW = require ("../middlewares/commonMiddlewares")
 
-router.get("/test-me", function (req, res) {
+router.get("/test-me1", function (req, res) {
     res.send("My first ever api!")
 })
+
+router.get("/test-me2", function (req, res) {
+    res.send("My first ever api!")
+})
+
+router.get("/test-me3", function (req, res) {
+    res.send("My first ever api!")
+})
+
+router.get("/test-me4", function (req, res) {
+    res.send("My first ever api!")
+})
+
 
 
 
